201eb5aa10f46d7378543d06797022bad3ec01ee
RonaliRanasinghe <ronaliranasinghe90@gmail.com>
2025-12-22 11:23:52 +0530

    modify

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -9,7 +9,15 @@ import { RootNavigator } from "./src/navigation/RootNavigator";
 import { BudgetProvider } from "./src/state/BudgetStore";
 import { theme } from "./src/theme/theme";
 
+import { registerForNotifications } from "./src/utils/registerNotifications";
+import "./src/utils/notificationHandler";
+
 export default function App() {
+
+  useEffect(() => {
+    registerForNotifications();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
@@ -23,3 +31,47 @@ export default function App() {
     </GestureHandlerRootView>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "react-native-gesture-handler";
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { GestureHandlerRootView } from "react-native-gesture-handler";
+// import { SafeAreaProvider } from "react-native-safe-area-context";
+// import { StatusBar } from "expo-status-bar";
+
+// import { RootNavigator } from "./src/navigation/RootNavigator";
+// import { BudgetProvider } from "./src/state/BudgetStore";
+// import { theme } from "./src/theme/theme";
+
+// export default function App() {
+//   return (
+//     <GestureHandlerRootView style={{ flex: 1 }}>
+//       <SafeAreaProvider>
+//         <BudgetProvider>
+//           <NavigationContainer theme={theme.navigationTheme}>
+//             <StatusBar style="light" />
+//             <RootNavigator />
+//           </NavigationContainer>
+//         </BudgetProvider>
+//       </SafeAreaProvider>
+//     </GestureHandlerRootView>
+//   );
+// }
